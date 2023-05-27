8afabe11361a5242eb5a578a51c4083b1746a39d
bathfire <>
2023-05-27 20:40:15 +0100

    update form style and csrf protect support

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_login import LoginManager
 from flask_ckeditor import CKEditor
 from flask_socketio import SocketIO
 from flask_caching import Cache
+from flask_wtf.csrf import CSRFProtect
 socketio = SocketIO(async_mode='eventlet')
 db= SQLAlchemy()
 login_manager = LoginManager()
@@ -12,6 +13,7 @@ cache = Cache(config={
         "CACHE_TYPE": "SimpleCache",
         "CACHE_DEFAULT_TIMEOUT": 300
     })
+csrf= CSRFProtect()
 app=Flask(__name__)
 def create_app(make_db=False,debug=False) -> Flask:
 
@@ -24,7 +26,7 @@ def create_app(make_db=False,debug=False) -> Flask:
     # suppress SQLAlchemy warning
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    app.config['SECRET_KEY'] = 'lalaowow'
+    app.config['SECRET_KEY'] = 'IL0veCh1psJustL1keY0u'
 
     basedir = os.path.abspath(os.path.dirname(__file__))
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.db")
@@ -52,6 +54,7 @@ def create_app(make_db=False,debug=False) -> Flask:
     socketio.init_app(app)
     login_manager.init_app(app)
     cache.init_app(app)
+    csrf.init_app(app)
     return app
 
 from . import models
